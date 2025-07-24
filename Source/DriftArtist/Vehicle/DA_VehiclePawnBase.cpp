01a147bd6a99614199eb6c654d4d1d29b8be3748
Z-HongYuan <1939218189@qq.com>
2025-07-24 15:15:04 +0800

    添加新摄像机系统组件:使用BP设置 添加技能系统组件:自定义的ASC组件 添加平滑同步组件:直接使用

--- Source/DriftArtist/Vehicle/DA_VehiclePawnBase.cpp
+++ Source/DriftArtist/Vehicle/DA_VehiclePawnBase.cpp
@@ -3,11 +3,91 @@
 
 #include "DA_VehiclePawnBase.h"
 
+
 ADA_VehiclePawnBase::ADA_VehiclePawnBase()
 {
-	
+	//摄像机系统
 	GameplayCamera = CreateDefaultSubobject<UGameplayCameraComponent>(TEXT("GameplayCameraComponent"));
 	GameplayCamera->SetupAttachment(GetMesh());
 
+	//开启模拟物理
 	GetMesh()->SetSimulatePhysics(true);
+
+	//技能系统
+	AbilitySystemComponent = CreateDefaultSubobject<UDA_AbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AttributeSet = CreateDefaultSubobject<UDA_AttributeSetBase>(TEXT("AttributeSet"));
+
+	//平滑同步
+	SmoothSync = CreateDefaultSubobject<USmoothSync>(TEXT("SmoothSync"));
+	SetReplicatingMovement(false);
+	
+}
+
+void ADA_VehiclePawnBase::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	if (AbilitySystemComponent)
+	{
+		InitAbilitySystem();
+	}
+	SetOwner(NewController);
+}
+
+void ADA_VehiclePawnBase::OnRep_Controller()
+{
+	Super::OnRep_Controller();
+}
+
+void ADA_VehiclePawnBase::InitAbilitySystem()
+{
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+
+	InitStartAbilitiesAndEffects();
+}
+
+void ADA_VehiclePawnBase::InitStartAbilitiesAndEffects()
+{
+	//初始默认属性
+	if (GetAbilitySystemComponent() and DefaultAttributeSet)
+	{
+		GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(
+			*GetAbilitySystemComponent()->MakeOutgoingSpec(DefaultAttributeSet, -1, GetAbilitySystemComponent()->MakeEffectContext()).Data.Get(),
+			GetAbilitySystemComponent()
+		);
+	}
+
+	//StartUp GE
+	for (TSubclassOf<UGameplayEffect>& Effect : StartupEffects)
+	{
+		if (!Effect) continue;
+		FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+		FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(Effect, -1, ContextHandle);
+		GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), GetAbilitySystemComponent());
+	}
+
+	//服务端Give
+	if (HasAuthority())
+	{
+		for (TSubclassOf<UGameplayAbility>& Ability : StartupAbilities)
+		{
+			if (!Ability) continue;
+			FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(Ability, -1, -1, this);
+			GetAbilitySystemComponent()->GiveAbilityWithTag(AbilitySpec);
+		}
+	}
+
+	//服务端Give
+	if (HasAuthority())
+	{
+		for (TSubclassOf<UGameplayAbility>& Ability : StartupDoOnceAbilities)
+		{
+			if (!Ability) continue;
+			FGameplayAbilitySpec AbilitySpec(Ability, -1, -1, this);
+			GetAbilitySystemComponent()->GiveAbilityAndActivateOnce(AbilitySpec);
+		}
+	}
 }
--- Source/DriftArtist/Vehicle/DA_VehiclePawnBase.h
+++ Source/DriftArtist/Vehicle/DA_VehiclePawnBase.h
@@ -3,7 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "SmoothSync.h"
 #include "WheeledVehiclePawn.h"
+#include "DriftArtist/AbilitySystem/DA_AbilitySystemComponent.h"
+#include "DriftArtist/AbilitySystem/Attribute/DA_AttributeSetBase.h"
 #include "GameFramework/GameplayCameraComponent.h"
 #include "DA_VehiclePawnBase.generated.h"
 
@@ -11,17 +15,60 @@
  * 添加GAS系统的载具,拥有技能释放功能,可以直接用于车辆,也可以继承后添加修改功能
  */
 UCLASS()
-class DRIFTARTIST_API ADA_VehiclePawnBase : public AWheeledVehiclePawn
+class DRIFTARTIST_API ADA_VehiclePawnBase : public AWheeledVehiclePawn, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	ADA_VehiclePawnBase();
 
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_Controller() override;
+
+	//交给谁初始化?
+	virtual UDA_AbilitySystemComponent* GetAbilitySystemComponent() const override { return AbilitySystemComponent; }
+	UDA_AttributeSetBase* GetAttributeSet() const { return AttributeSet; }
+
+	//用于初始化技能系统,可能包含属性初始化
+	UFUNCTION(BlueprintCallable)
+	void InitAbilitySystem();
+
+	void InitStartAbilitiesAndEffects();
+
 protected:
-	//摄像机系统
+	/*
+	 * 摄像机系统
+	 * 没有需要自定义的地方,直接使用蓝图设置即可
+	 */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UGameplayCameraComponent> GameplayCamera;
 
+	/*
+	 * 技能系统
+	 * 
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UDA_AbilitySystemComponent> AbilitySystemComponent;
+	UPROPERTY()
+	TObjectPtr<UDA_AttributeSetBase> AttributeSet;
+
+	//默认属性GE
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TSubclassOf<UGameplayEffect> DefaultAttributeSet;
+
+	//初始GE
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TArray<TSubclassOf<UGameplayEffect>> StartupEffects;
+
+	//初始技能
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
+
+	//初始执行一次技能
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TArray<TSubclassOf<UGameplayAbility>> StartupDoOnceAbilities;
+
 private:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USmoothSync> SmoothSync;
 };
