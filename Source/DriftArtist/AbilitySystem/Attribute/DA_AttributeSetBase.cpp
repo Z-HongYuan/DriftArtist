d326a51475aae5b3c2b8856b0e8e7d14d265faa0
Z-HongYuan <1939218189@qq.com>
2025-07-24 11:12:01 +0800

    构建基础的Attribute属性集

---
+++ Source/DriftArtist/AbilitySystem/Attribute/DA_AttributeSetBase.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DA_AttributeSetBase.h"
+#include "GameplayEffectExtension.h"
+#include "Net/UnrealNetwork.h"
+
+UDA_AttributeSetBase::UDA_AttributeSetBase()
+{
+}
+
+void UDA_AttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	/*`REPNOTIFY_Always` 告诉 `OnRep` 函数在本地值已经等于从服务器复制的值（由于预测）时触发。默认情况下，如果本地值与从服务器复制的值相同，则不会触发 `OnRep` 函数。*/
+	DOREPLIFETIME_CONDITION_NOTIFY(UDA_AttributeSetBase, Health, COND_None, REPNOTIFY_Always); //终身复制
+}
+
+void UDA_AttributeSetBase::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+}
+
+void UDA_AttributeSetBase::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
+	}
+}
+
+void UDA_AttributeSetBase::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDA_AttributeSetBase, Health, OldHealth);
+}
+
+void UDA_AttributeSetBase::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDA_AttributeSetBase, MaxHealth, OldMaxHealth);
+}
---
+++ Source/DriftArtist/AbilitySystem/Attribute/DA_AttributeSetBase.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "DA_AttributeSetBase.generated.h"
+
+// 属性访问宏,Attribute提供的快捷构建器,用于创建Getter和Setter
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+// ATTRIBUTE_ACCESSORS(UMyHealthSet, Health) 属性中需要使用的快捷宏
+
+/**
+ * 基础的属性集示例
+ * 包括Health和MaxHealth
+ * 带有车辆需要的基础属性
+ */
+UCLASS()
+class DRIFTARTIST_API UDA_AttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UDA_AttributeSetBase();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UDA_AttributeSetBase, Health)
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UDA_AttributeSetBase, MaxHealth)
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+};
