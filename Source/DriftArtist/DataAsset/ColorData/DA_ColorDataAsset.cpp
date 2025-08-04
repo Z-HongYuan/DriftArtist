6cfc2c325eeda438f3569e410f4c10d2104fc9c8
Z-HongYuan <1939218189@qq.com>
2025-08-04 15:13:49 +0800

    BETA 0.2

---
+++ Source/DriftArtist/DataAsset/ColorData/DA_ColorDataAsset.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DA_ColorDataAsset.h"
+
+FLinearColor UDA_ColorDataAsset::GetColorFromTag(const FGameplayTag& InTag) const
+{
+	for (const FDA_ColorTagData& ColorData : ColorWithTag)
+	{
+		if (ColorData.Tag == InTag)
+		{
+			return ColorData.Color;
+		}
+	}
+	return FLinearColor::White;
+}
+
+FGameplayTag UDA_ColorDataAsset::GetTagFromColor(const FLinearColor& InColor) const
+{
+	for (const FDA_ColorTagData& ColorData : ColorWithTag)
+	{
+		if (ColorData.Color == InColor)
+		{
+			return ColorData.Tag;
+		}
+	}
+	return FGameplayTag();
+}
---
+++ Source/DriftArtist/DataAsset/ColorData/DA_ColorDataAsset.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "DA_ColorDataAsset.generated.h"
+
+//颜色与Tag对应
+USTRUCT(BlueprintType)
+struct FDA_ColorTagData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayTag Tag = FGameplayTag();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLinearColor Color;
+};
+
+/**
+ * 用于颜色与Tag对应
+ */
+UCLASS()
+class DRIFTARTIST_API UDA_ColorDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure)
+	FLinearColor GetColorFromTag(const FGameplayTag& InTag) const;
+
+	UFUNCTION(BlueprintPure)
+	FGameplayTag GetTagFromColor(const FLinearColor& InColor) const;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FDA_ColorTagData> ColorWithTag;
+};
